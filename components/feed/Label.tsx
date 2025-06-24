e6cdf2df6db9e1b21069001d6f33d9863bdf0699
Joonsu Yoon <bananamilk452@gmail.com>
2025-06-24 20:27:16 +0900

    Contents Warning 라벨 추가

---
+++ components/feed/Label.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { Label } from "@atproto/api";
+import { CircleAlertIcon } from "lucide-react";
+import { useState } from "react";
+
+const CONTENT_WARNING = {
+  sexual: "성인 콘텐츠",
+  nudity: "나체",
+  porn: "성인 콘텐츠",
+  "graphic-media": "불쾌감을 주는 미디어",
+} as Record<string, string>;
+
+export function FeedLabel({
+  labels,
+  children,
+}: {
+  labels?: Label[];
+  children?: React.ReactNode;
+}) {
+  const [show, setShow] = useState(false);
+
+  if (!labels || labels.length === 0) {
+    return children;
+  }
+
+  const title = labels
+    .map((label) => CONTENT_WARNING[label.val])
+    .filter(Boolean)
+    .join(", ");
+
+  function handleClick() {
+    setShow(!show);
+  }
+
+  return (
+    <div className="flex flex-col gap-1">
+      <div className="flex items-center gap-3 rounded-lg bg-black/30 p-4">
+        <CircleAlertIcon className="size-6 text-gray-400" />
+        <p className="flex-grow text-sm font-semibold text-gray-400">{title}</p>
+        <button
+          className="cursor-pointer text-sm hover:underline"
+          onClick={handleClick}
+        >
+          {show ? "숨기기" : "표시"}
+        </button>
+      </div>
+      {show && children}
+    </div>
+  );
+}
