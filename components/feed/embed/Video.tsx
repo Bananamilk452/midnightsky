da8cab82c68181a19031bc38cb34ad3b4df42203
Joonsu Yoon <bananamilk452@gmail.com>
2025-06-24 22:05:29 +0900

    video autoplay 제거 및 버그 해결

--- components/feed/embed/Video.tsx
+++ components/feed/embed/Video.tsx
@@ -2,19 +2,18 @@
 
 import { AppBskyEmbedVideo } from "@atproto/api";
 import Hls from "hls.js";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 export function FeedVideo({ content }: { content: AppBskyEmbedVideo.View }) {
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  if (videoRef.current) {
-    const hls = new Hls();
-    hls.loadSource(content.playlist);
-    hls.attachMedia(videoRef.current);
-    hls.on(Hls.Events.MANIFEST_PARSED, () => {
-      videoRef.current?.play();
-    });
-  }
+  useEffect(() => {
+    if (videoRef.current) {
+      const hls = new Hls();
+      hls.loadSource(content.playlist);
+      hls.attachMedia(videoRef.current);
+    }
+  }, [content.playlist]);
 
   return (
     <div className="mt-2">
@@ -23,7 +22,6 @@ export function FeedVideo({ content }: { content: AppBskyEmbedVideo.View }) {
         className="h-auto max-h-[515px] rounded-lg border border-white/30"
         controls
         muted
-        autoPlay
         playsInline
       />
     </div>
