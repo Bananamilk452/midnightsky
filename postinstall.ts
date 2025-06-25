efed264a11a61174a99f25a01f6d64805952753c
Joonsu Yoon <bananamilk452@gmail.com>
2025-06-25 22:43:16 +0900

    tinymce 추가

---
+++ postinstall.ts
@@ -0,0 +1,10 @@
+import path from "path";
+import fse from "fs-extra";
+
+const topDir = __dirname;
+fse.emptyDirSync(path.join(topDir, "public", "tinymce"));
+fse.copySync(
+  path.join(topDir, "node_modules", "tinymce"),
+  path.join(topDir, "public", "tinymce"),
+  { overwrite: true, dereference: true },
+);
