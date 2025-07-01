3ebb9f7845e785c9335966bb6422907ce985bf29
Joonsu Yoon <bananamilk452@gmail.com>
2025-07-01 12:24:42 +0900

    FeedFooter 간단하게

--- components/feed/Footer.tsx
+++ components/feed/Footer.tsx
@@ -4,6 +4,7 @@ import {
   HeartIcon,
   MessageSquareIcon,
   Repeat2Icon,
+  ShareIcon,
 } from "lucide-react";
 
 import { cn } from "@/lib/utils";
@@ -16,39 +17,42 @@ export function FeedFooter({
   className?: string;
 }) {
   return (
-    <div className={cn("grid grid-cols-4", className)}>
-      <div className="flex items-center">
-        <button
-          onClick={(e) => e.stopPropagation()}
-          className="flex items-center gap-1.5 rounded-full p-1 text-gray-400 hover:cursor-pointer hover:bg-white/10"
-        >
-          <MessageSquareIcon className="size-4" />
-          {post.replyCount && post.replyCount > 0 ? post.replyCount : ""}
-        </button>
-      </div>
-      <div className="flex items-center">
-        <button
-          onClick={(e) => e.stopPropagation()}
-          className="flex items-center gap-1.5 rounded-full p-1 text-gray-400 hover:cursor-pointer hover:bg-white/10"
-        >
-          <Repeat2Icon className="size-5" />
-          {post.repostCount && post.repostCount > 0 ? post.repostCount : ""}
-        </button>
-      </div>
-      <div className="flex items-center">
-        <button
-          onClick={(e) => e.stopPropagation()}
-          className="flex items-center gap-1.5 rounded-full p-1 text-gray-400 hover:cursor-pointer hover:bg-white/10"
-        >
-          <HeartIcon className="size-4" />
-          {post.likeCount && post.likeCount > 0 ? post.likeCount : ""}
-        </button>
-      </div>
-      <div className="flex items-center">
-        <button onClick={(e) => e.stopPropagation()} className="text-gray-400">
-          <EllipsisIcon className="size-4" />
-        </button>
-      </div>
+    <div className={cn("flex items-center justify-between", className)}>
+      <FeedFooterButton>
+        <MessageSquareIcon className="size-4" />
+        {post.replyCount && post.replyCount > 0 ? post.replyCount : ""}
+      </FeedFooterButton>
+      <FeedFooterButton>
+        <Repeat2Icon className="size-5" />
+        {post.repostCount && post.repostCount > 0 ? post.repostCount : ""}
+      </FeedFooterButton>
+      <FeedFooterButton>
+        <HeartIcon className="size-4" />
+        {post.likeCount && post.likeCount > 0 ? post.likeCount : ""}
+      </FeedFooterButton>
+      <FeedFooterButton>
+        <ShareIcon className="size-4" />
+      </FeedFooterButton>
+      <FeedFooterButton>
+        <EllipsisIcon className="size-4" />
+      </FeedFooterButton>
     </div>
+  );
+}
+
+export function FeedFooterButton({
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}) {
+  return (
+    <button
+      onClick={onClick}
+      className="flex items-center gap-1.5 rounded-full p-1 text-gray-400 hover:cursor-pointer hover:bg-white/10"
+    >
+      {children}
+    </button>
   );
 }
