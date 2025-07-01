44117353c25fcf6cb7342053fee64e9b30a23ec4
Joonsu Yoon <bananamilk452@gmail.com>
2025-07-01 13:32:41 +0900

    postinstall에 tinymce lang 추가

--- postinstall.ts
+++ postinstall.ts
@@ -8,3 +8,8 @@ fse.copySync(
   path.join(topDir, "public", "tinymce"),
   { overwrite: true, dereference: true },
 );
+fse.copySync(
+  path.join(topDir, "public", "langs"),
+  path.join(topDir, "public", "tinymce", "langs"),
+  { overwrite: true, dereference: true },
+);
