d193a2593826a2679a6e6e4b88967b898c33a44d
Joonsu Yoon <bananamilk452@gmail.com>
2025-07-07 15:21:01 +0900

    ErrorBoundaryPage 추가

---
+++ components/ErrorBoundaryPage.tsx
@@ -0,0 +1,32 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export function ErrorBoundaryPage({
+  error,
+  onReset,
+}: {
+  error: any;
+  onReset: () => void;
+}) {
+  return (
+    <div className="flex size-full items-center justify-center">
+      <Card>
+        <CardHeader>
+          <CardTitle>에러가 발생했어요!</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <pre style={{ whiteSpace: "normal" }}>{error.message}</pre>
+        </CardContent>
+        <CardFooter>
+          <Button onClick={onReset}>다시 시도</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
