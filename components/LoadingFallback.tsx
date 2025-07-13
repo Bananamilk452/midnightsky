b8e3f02d35fd36edc9137d16193ead8324172823
Joonsu Yoon <bananamilk452@gmail.com>
2025-07-13 13:06:27 +0900

    LoadingFallback 크기 수정

--- components/LoadingFallback.tsx
+++ components/LoadingFallback.tsx
@@ -2,7 +2,7 @@ import { Spinner } from "@/components/Spinner";
 
 export function LoadingFallback() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex h-32 items-center justify-center">
       <Spinner className="size-6" />
     </div>
   );
