22169bcbe8835bf0330f7f29235b633368e296e6
Joonsu Yoon <bananamilk452@gmail.com>
2025-08-06 20:49:35 +0900

    HomeSidebar, HomeSidemenu 추가 및 DefaultLayout에 추가

---
+++ components/home/HomeSidebar.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { HomeIcon, LogOutIcon } from "lucide-react";
+import Link from "next/link";
+
+import { Avatar } from "@/components/primitive/Avatar";
+import { Spinner } from "@/components/Spinner";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarSeparator,
+} from "@/components/ui/sidebar";
+import { signOut } from "@/lib/bluesky/action";
+import { useSession } from "@/lib/hooks/useBluesky";
+
+export function HomeSidebar() {
+  const { data: user } = useSession();
+
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          {!user ? (
+            <Spinner className="size-6 p-4" />
+          ) : (
+            <div className="flex flex-col items-start p-4 pb-0">
+              <Avatar
+                className="size-11"
+                src={user.avatar}
+                alt={user.displayName || user.handle}
+              />
+              <h2 className="mt-2 w-full overflow-hidden text-ellipsis font-semibold">
+                {user.displayName || user.handle}
+              </h2>
+              <h3 className="w-full overflow-hidden text-ellipsis text-sm text-gray-400">
+                @{user.handle}
+              </h3>
+            </div>
+          )}
+        </SidebarGroup>
+        <div className="pl-4 pr-6">
+          <SidebarSeparator />
+        </div>
+        <SidebarGroup className="p-4 pt-0">
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild className="h-12">
+                <Link href="/home">
+                  <HomeIcon className="!size-6" />
+                  <span className="text-lg font-semibold">홈</span>
+                </Link>
+              </SidebarMenuButton>
+              <SidebarMenuButton className="h-12" onClick={signOut}>
+                <LogOutIcon className="!size-6" />
+                <span className="text-lg font-semibold">로그아웃</span>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
--- components/layouts/Default.tsx
+++ components/layouts/Default.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 
+import { HomeSidebar } from "@/components/home/HomeSidebar";
+import { HomeSidemenu } from "@/components/home/HomeSidemenu";
 import { WriterButton } from "@/components/home/WriterButton";
 import { WriterProvider } from "@/components/providers/WriterProvider";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 export function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
     <div id="root" className="h-dvh w-dvw overflow-auto">
-      <div className="relative mx-auto min-h-full max-w-[600px] bg-black/50">
+      <SidebarProvider defaultOpen={false}>
         <WriterProvider>
-          {children}
-          <WriterButton />
+          <HomeSidemenu />
+          <HomeSidebar />
+          <div className="relative mx-auto min-h-full w-full max-w-[600px] bg-black/50">
+            {children}
+            <WriterButton />
+          </div>
         </WriterProvider>
-      </div>
+      </SidebarProvider>
     </div>
   );
 }
