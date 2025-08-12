93d105a1a72dfcd2d1022dba43a7f21f6c35b403
Joonsu Yoon <bananamilk452@gmail.com>
2025-08-12 13:33:28 +0900

    프로필 페이지 추가

--- app/profile/[authority]/page.tsx
+++ app/profile/[authority]/page.tsx
@@ -1,11 +1,29 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import React from "react";
 
 import { ErrorBoundaryPage } from "@/components/ErrorBoundaryPage";
+import { Feed } from "@/components/feed";
+import { InfiniteScrollTrigger } from "@/components/InfiniteScrollTrigger";
 import { LoadingFallback } from "@/components/LoadingFallback";
 import { ProfileBanner } from "@/components/profile/Banner";
-import { useProfile } from "@/lib/hooks/useBluesky";
+import { useAuthorFeed, useProfile } from "@/lib/hooks/useBluesky";
+import { createFeedKey } from "@/lib/utils";
+
+type FeedData = NonNullable<ReturnType<typeof useAuthorFeed>["data"]>;
+const FeedList = React.memo(({ feeds }: { feeds: FeedData }) => (
+  <>
+    {feeds.pages.map((group, i) => (
+      <React.Fragment key={i}>
+        {group.feed.map((feed) => (
+          <Feed key={createFeedKey(feed)} feed={feed} />
+        ))}
+      </React.Fragment>
+    ))}
+  </>
+));
+FeedList.displayName = "FeedList";
 
 export default function Page() {
   const { authority } = useParams();
@@ -14,15 +32,45 @@ export default function Page() {
     throw new Error("Invalid parameters");
   }
 
-  const { data, error, status } = useProfile(decodeURIComponent(authority));
+  const at = decodeURIComponent(authority);
+  const { data, error, status } = useProfile(at);
 
-  return status === "pending" ? (
+  const {
+    data: authorFeed,
+    error: authorFeedError,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    status: authorFeedStatus,
+    refetch,
+  } = useAuthorFeed({
+    actor: at,
+    limit: 30,
+  });
+
+  return status === "pending" && authorFeedStatus === "pending" ? (
     <LoadingFallback />
-  ) : status === "error" ? (
-    <ErrorBoundaryPage error={error} />
+  ) : status === "error" || authorFeedStatus === "error" ? (
+    <ErrorBoundaryPage error={error || authorFeedError} onReset={refetch} />
   ) : (
     <>
-      <ProfileBanner profile={data} />
+      {status === "success" && (
+        <>
+          <ProfileBanner profile={data} />
+          <hr />
+        </>
+      )}
+
+      {authorFeedStatus === "success" && (
+        <>
+          <FeedList feeds={authorFeed} />
+          <InfiniteScrollTrigger
+            onTrigger={fetchNextPage}
+            hasNextPage={hasNextPage}
+            isFetching={isFetching}
+          />
+        </>
+      )}
     </>
   );
 }
