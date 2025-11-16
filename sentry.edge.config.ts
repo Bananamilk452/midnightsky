51b80acc09b1e8304388bdcb042ad58c81a85c1c
Seoa Yoon <bananamilk452@gmail.com>
2025-11-16 17:46:22 +0900

    Sentry 엣지에서 prisma integration 제거

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -7,7 +7,6 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://7c223e15cd1e3112446dac79034c7c7d@o409411.ingest.us.sentry.io/4510372826644480",
-  integrations: [Sentry.prismaIntegration()],
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
